94eb6d227b677463b98eab02c0e00c00dfdc3aa7
FOÏS Grégory <foisgregory38@gmail.com>
2023-04-05 14:20:23 +0200

    Exo 19

--- Question_19.php
+++ Question_19.php
@@ -1,45 +1,48 @@
 <?php
+
 class Technician {
-    private string $name;
     private ?Technician $superior = null;
     private array $subordinates = [];
 
-    public function __construct(string $name) {
-        $this->name = $name;
-    }
-
-    public function setSuperior(?Technician $superior) : void {
-        if($superior === $this) {
-            throw new InvalidArgumentException("A technician cannot be his own superior.");
+    public function setSuperior(?Technician $superior): Technician {
+        if ($superior === $this) {
+            throw new Exception("A technician cannot be their own superior.");
+        }
+        if ($this->superior !== null) {
+            $this->superior->removeSubordinate($this);
         }
         $this->superior = $superior;
-        $superior->addSubordinate($this);
-    }
-
-    public function addSubordinate(Technician $subordinate) : void {
-        if($subordinate === $this) {
-            throw new InvalidArgumentException("A technician cannot be his own subordinate.");
+        if ($superior !== null) {
+            $superior->addSubordinate($this);
         }
-        $this->subordinates[] = $subordinate;
-        $subordinate->setSuperior($this);
+        return $this;
     }
 
-    public function getSuperior() : ?Technician {
+    public function getSuperior(): ?Technician {
         return $this->superior;
     }
 
-    public function getSubordinates() : array {
+    public function addSubordinate(Technician $subordinate): Technician {
+        if ($subordinate === $this) {
+            throw new Exception("A technician cannot be their own subordinate.");
+        }
+        if (!in_array($subordinate, $this->subordinates)) {
+            $this->subordinates[] = $subordinate;
+            $subordinate->setSuperior($this);
+        }
+        return $this;
+    }
+
+    public function removeSubordinate(Technician $subordinate): Technician {
+        $key = array_search($subordinate, $this->subordinates, true);
+        if ($key !== false) {
+            array_splice($this->subordinates, $key, 1);
+            $subordinate->setSuperior(null);
+        }
+        return $this;
+    }
+
+    public function getSubordinates(): array {
         return $this->subordinates;
     }
 }
-
-// Exemple d'utilisation
-$john = new Technician("John");
-$jane = new Technician("Jane");
-$bob = new Technician("Bob");
-
-$john->setSuperior($jane);
-$jane->addSubordinate($bob);
-
-var_dump($john->getSuperior()); // Output: object(Technician)#2 (1) {...}
-var_dump($jane->getSubordinates()); // Output: array(1) { [0]=> object(Technician)#3 (1) {...} }
